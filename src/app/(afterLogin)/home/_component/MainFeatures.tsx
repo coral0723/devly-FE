0b7e9e31432469a99330b809bb6cc2d28b1651e5
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2025-01-25 17:25:18 +0900

    feat: MeainFeatures 컴포넌트 skeleton 추가

--- src/app/(afterLogin)/home/_component/MainFeatures.tsx
+++ src/app/(afterLogin)/home/_component/MainFeatures.tsx
@@ -10,28 +10,36 @@ import {
 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { getTodayTasks } from '../_lib/getTodayTasks';
+import MainFeaturesSkeleton from './skeleton/MainFeaturesSkeleton';
 
 export default function MainFeatures() {
   const router = useRouter();
 
-  const {data: tasks} = useQuery<Tasks, object, Tasks, [_1: string]>({
+  const {data: tasks, isLoading} = useQuery<Tasks, object, Tasks, [_1: string]>({
     queryKey: ['today-tasks'],
     queryFn: getTodayTasks,
     staleTime: 60 * 1000,
     gcTime: 300 * 1000,
   });
 
+  //로딩 중이거나 데이터가 없다면 Skeleton을 보여줌
+  if(isLoading || !tasks) {
+    return (
+      <MainFeaturesSkeleton/>
+    )
+  }
+
   return (
     <div className="grid grid-cols-2 gap-4">
       {/* 단어 학습 */}
       <div
           className={`bg-white rounded-2xl border border-gray-200 p-5 space-y-3 transition-all
-            ${tasks?.word.completed === false && "cursor-pointer hover:bg-emerald-50 hover:border-emerald-200 active:scale-95"}`}
-          onClick={tasks?.word.completed ? undefined : (() => {router.replace(`/words?groupId=${tasks?.word.groupId}`)})}
+            ${tasks.word.completed === false && "cursor-pointer hover:bg-emerald-50 hover:border-emerald-200 active:scale-95"}`}
+          onClick={tasks.word.completed ? undefined : (() => {router.replace(`/words?groupId=${tasks.word.groupId}`)})}
       >
         <div className="flex items-center justify-between">
           <BookOpen size={32} className="text-emerald-500"/>
-          {tasks?.word.completed === false ?(
+          {tasks.word.completed === false ?(
             <span className="text-sm px-3 py-1.5 bg-emerald-100 text-emerald-600 rounded-full animate-pulse">
               단어 {tasks.word.total}개
             </span>
@@ -48,8 +56,8 @@ export default function MainFeatures() {
       {/* 개발 지식 */}
       <div
           className={`bg-white rounded-2xl border border-gray-200 p-5 space-y-3 transition-all
-            ${tasks?.knowledge.completed === false && "cursor-pointer hover:bg-blue-50 hover:border-blue-200 active:scale-95"}`}
-          onClick={tasks?.knowledge.completed ? undefined : (() => {router.replace(`/knowledge?groupId=${tasks?.knowledge.groupId}`)})}
+            ${tasks.knowledge.completed === false && "cursor-pointer hover:bg-blue-50 hover:border-blue-200 active:scale-95"}`}
+          onClick={tasks.knowledge.completed ? undefined : (() => {router.replace(`/knowledge?groupId=${tasks.knowledge.groupId}`)})}
       >
         <div className="flex items-center justify-between">
           <Lightbulb size={32} className="text-blue-500"/>
@@ -70,12 +78,12 @@ export default function MainFeatures() {
       {/* 모의 PR */}
       <div
           className={`bg-white rounded-2xl border border-gray-200 p-5 space-y-2 transition-all
-            ${tasks?.pr.completed === false && "cursor-pointer hover:bg-purple-50 hover:border-purple-200 active:scale-95"}`}
-          onClick={tasks?.pr.completed ? undefined : (() => {router.replace(`/pr?groupId=${tasks?.pr.groupId}`)})}
+            ${tasks.pr.completed === false && "cursor-pointer hover:bg-purple-50 hover:border-purple-200 active:scale-95"}`}
+          onClick={tasks.pr.completed ? undefined : (() => {router.replace(`/pr?groupId=${tasks.pr.groupId}`)})}
       >
         <div className="flex items-center justify-between">
           <GitPullRequest size={24} className="text-purple-500"/>
-          {tasks?.pr.completed === false ?(
+          {tasks.pr.completed === false ?(
             <span className="text-sm px-3 py-1.5 bg-purple-100 text-purple-600 rounded-full animate-pulse">
               PR {tasks.pr.total}개
             </span>
@@ -92,12 +100,12 @@ export default function MainFeatures() {
       {/* 모의 논의 */}
       <div
           className={`bg-white rounded-2xl border border-gray-200 p-5 space-y-2 transition-all
-            ${tasks?.discussion.completed === false && "cursor-pointer hover:bg-orange-50 hover:border-orange-200 active:scale-95"}`}
-          onClick={tasks?.discussion.completed ? undefined : (() => {router.replace('/discussion')})}
+            ${tasks.discussion.completed === false && "cursor-pointer hover:bg-orange-50 hover:border-orange-200 active:scale-95"}`}
+          onClick={tasks.discussion.completed ? undefined : (() => {router.replace('/discussion')})}
       >
         <div className="flex items-center justify-between">
           <MessageSquare size={24} className="text-orange-500"/>
-          {tasks?.discussion.completed === false ?(
+          {tasks.discussion.completed === false ?(
             <span className="text-sm px-3 py-1.5 bg-orange-100 text-orange-600 rounded-full animate-pulse">
               주제 {tasks.discussion.total}개
             </span>
---
+++ src/app/(afterLogin)/home/_component/skeleton/MainFeaturesSkeleton.tsx
@@ -0,0 +1,18 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function MainFeaturesSkeleton() {
+  return (
+    <div className='grid grid-cols-2 gap-4'>
+      {[1,2,3,4].map((i) => (
+        <div key={i} className='bg-white rounded-2xl border border-gray-200 p-5 space-y-3'>
+          <div className="flex items-center justify-between">
+            <Skeleton className="w-8 h-8 rounded-full" />
+            <Skeleton className="w-20 h-8 rounded-full" />
+          </div>
+          <Skeleton className="w-24 h-6 rounded-md" />
+          <Skeleton className="w-48 h-5 rounded-md" />
+        </div>
+      ))}
+    </div>
+  )
+}
