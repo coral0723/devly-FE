cb4105d412689a4146331d50697d4a475877d216
san9901 <san990190@gmail.com>
2025-02-03 15:59:54 +0900

    style: 채팅 화면 수정
    말풍선은 추후 다시 확인할 예정

--- src/app/(afterLogin)/discussion/[id]/_component/ChatMessage.tsx
+++ src/app/(afterLogin)/discussion/[id]/_component/ChatMessage.tsx
@@ -16,23 +16,25 @@ export default function ChatMessage({key, chat, isLoading}: Props) {
   return (
     <div 
       key={key} 
-      className={`flex mb-4 ${
+      className={`flex mb-4 items-start ${
         chat.role === 'ai' 
           ? 'justify-start' 
           : 'justify-end'
       }`}
     >
+      {chat.role === 'ai' && (
+        <div className="bg-orange-100 rounded-full p-2 mr-2 border-2 border-orange-400">
+          <Bot size={24} className="text-orange-400"/>
+        </div>
+      )}
       <div 
-        className={`p-4 rounded-xl max-h-96 overflow-y-auto w-fit max-w-[calc(100%-3rem)] ${
+        className={`py-3 px-3 max-h-96 overflow-y-auto w-fit max-w-[calc(100%-3rem)] ${
           chat.role === 'ai' 
-            ? 'bg-orange-100 border-orange-200' 
-            : 'bg-white'
+            ? 'bg-orange-100 border-orange-200 rounded-b-xl rounded-tr-xl' 
+            : 'bg-white rounded-b-xl rounded-tl-xl'
         }`}
       >
-        <div className="text-sm mb-1">
-          {chat.role === 'ai' && (<Bot size={24} className="text-orange-400"/>)}
-        </div>
-        <div className="whitespace-pre-wrap break-words">
+        <div className="whitespace-pre-wrap break-words text-sm">
           {isLoading ? (
             <LoadingSpinner size={"xs"}/>
           ) : 
