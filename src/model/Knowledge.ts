a937f4e0a04b6c0b15b1209989c1a48cadd8810e
san9901 <san990190@gmail.com>
2025-03-08 21:08:53 +0900

    feat: Knowledge model 수정

--- src/model/Knowledge.ts
+++ src/model/Knowledge.ts
@@ -1,11 +1,28 @@
+export interface Distractor {
+  id: number;
+  distractor: string;
+}
+
 export interface Knowledge {
   id: number;
   title: string;
   content: string;
-  practice: {
-    question: string;
-    options: string[];
+  quiz: {
+    text: string;
+    distractors: Distractor[];
     answer: number;
   };
   code: string;
 }
+
+// export interface Knowledge {
+//   id: number;
+//   title: string;
+//   content: string;
+//   practice: {
+//     question: string;
+//     options: string[];
+//     answer: number;
+//   };
+//   code: string;
+// }
