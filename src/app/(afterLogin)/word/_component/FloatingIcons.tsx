d27ca11d1d236314030e8c6071aeb1a874568b59
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2025-08-07 17:30:03 +0900

    style: FloatingIcon 위치 수정
    - 모바일 기기에서 하단의 FloatingIcon이 안 보이는 현상 수정

--- src/app/(afterLogin)/word/_component/FloatingIcons.tsx
+++ src/app/(afterLogin)/word/_component/FloatingIcons.tsx
@@ -11,12 +11,12 @@ export default function FloatingIcons() {
           <polyline points="16 18 22 12 16 6"/><polyline points="8 6 2 12 8 18"/>
         </svg>
       </div>
-      <div className="absolute bottom-40 left-10 animate-float-fast opacity-10">
+      <div className="absolute bottom-36 left-10 animate-float-fast opacity-10">
         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#A855F7" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
           <polyline points="4 17 10 11 4 5"/><line x1="12" x2="20" y1="19" y2="19"/>
         </svg>
       </div>
-      <div className="absolute bottom-40 right-10 animate-float-medium opacity-10">
+      <div className="absolute bottom-36 right-10 animate-float-medium opacity-10">
         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#F97316" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
           <path d="M10 12.5 8 15l2 2.5"/><path d="m14 12.5 2 2.5-2 2.5"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7z"/>
         </svg>
