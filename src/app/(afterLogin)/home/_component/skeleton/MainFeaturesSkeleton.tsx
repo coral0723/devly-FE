fe14f3832802675ac731121460b457910d802822
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2025-08-07 17:13:15 +0900

    fix: Skeleton 길이 수정
    - 모바일 화면에서 Skeleton의 길이가 안 줄어드는 문제 수정

--- src/app/(afterLogin)/home/_component/skeleton/MainFeaturesSkeleton.tsx
+++ src/app/(afterLogin)/home/_component/skeleton/MainFeaturesSkeleton.tsx
@@ -7,10 +7,10 @@ export default function MainFeaturesSkeleton() {
         <div key={i} className='bg-white rounded-2xl border border-gray-200 p-5 space-y-3'>
           <div className="flex items-center justify-between">
             <Skeleton className="w-8 h-8 rounded-full" />
-            <Skeleton className="w-20 h-8 rounded-full" />
+            <Skeleton className="min-w-[40px] max-w-[80px] w-full h-8 rounded-full" />
           </div>
           <Skeleton className="w-24 h-6 rounded-md" />
-          <Skeleton className="w-48 h-5 rounded-md" />
+          <Skeleton className="min-w-[104px] h-5 rounded-md" />
         </div>
       ))}
     </div>
