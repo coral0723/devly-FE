16a390f3916083d20fb84f165effdf0c6d39a6db
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2025-08-14 17:12:01 +0900

    style: pr  페이지 Header 요소 스타일 수정
    - step 가운데 정렬
    - 뒤로 가기 버튼 아이콘으로 변경
    - 변경된 파일 버튼 아이콘으로 변경

--- src/app/(afterLogin)/pr/[studyId]/[prId]/_component/Header.tsx
+++ src/app/(afterLogin)/pr/[studyId]/[prId]/_component/Header.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { X } from "lucide-react";
+import { ArrowLeft, FileCode2, X } from "lucide-react";
 
 type Props = {
   title: string;
@@ -17,33 +17,47 @@ export default function Header({title, currentStep, stepLength, setCurrentStep, 
       <div className="flex flex-col gap-3">
         <div className="flex items-center justify-between relative w-full">
           <div className="flex items-center gap-2 flex-1">
-            <button
-              onClick={onExit}
-              className="p-2 -ml-2 hover:bg-gray-100 rounded-full"
-            >
-              <X size={24}/>
-            </button>
-            {currentStep > 1 && (
+            {currentStep <= 1 ? (
               <button
-                onClick={() => setCurrentStep(currentStep - 1)}
-                className="text-sm px-3 py-1 border border-gray-300 rounded hover:bg-gray-50"
+                onClick={onExit}
+                className="p-2 -ml-2 hover:bg-gray-100 rounded-full"
               >
-                이전 단계
+                <X size={24}/>
               </button>
-            )}
+              ) : (
+                <div className="relative group inline-block">
+                  <button
+                    onClick={() => setCurrentStep(currentStep - 1)}
+                    className="p-2 -ml-2 hover:bg-gray-100 rounded-full"
+                  >
+                    <ArrowLeft size={24} className="text-purple-800"/>
+                  </button>
+                  <span className="absolute mb-2 left-1 transform translate-x-1/2 
+                    bg-purple-100 text-purple-800 text-sm rounded px-2 py-1 opacity-0 
+                    group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap">
+                    이전 단계
+                  </span>
+                </div>
+              )
+            }
           </div>
           <div className="flex-1 flex justify-center">
             <span className="px-2 py-1 text-sm bg-purple-100 text-purple-800 rounded-full">
               Step {currentStep}/{stepLength}
             </span>
           </div>
-          <div className="flex flex-1 justify-end gap-2">
+          <div className="flex flex-1 justify-end gap-2 relative group inline-block">
             <button
               onClick={() => setShowFiles(true)}
-              className="py-2 px-3 text-sm border border-gray-300 rounded hover:bg-gray-50"
+              className="p-2 -ml-2 hover:bg-gray-100 rounded-full"
             >
-              변경된 파일
+              <FileCode2 size={24} className="text-purple-800"/>
             </button>
+            <span className="absolute mb-2 transform -translate-x-1/2 
+              bg-purple-100 text-purple-800 text-sm rounded px-2 py-1 opacity-0 
+              group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap">
+              변경된 파일
+            </span>
           </div>
         </div>
         <h1 className="text-lg font-semibold text-gray-900">{title}</h1>
