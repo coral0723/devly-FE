ecba3d2301a4183760bb42c4c8143f7915ac06cd
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2025-08-18 19:50:13 +0900

    chore: 배포를 위한 settings 삭제

--- src/app/(afterLogin)/profile/_component/Header.tsx
+++ src/app/(afterLogin)/profile/_component/Header.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { ArrowLeft, Settings } from 'lucide-react';
+import { ArrowLeft } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
 export default function Header() {
