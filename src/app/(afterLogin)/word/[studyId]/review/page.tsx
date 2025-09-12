3836346aa22cd32685689bd34b6ec042ba5caf33
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2025-09-12 12:28:41 +0900

    chore: /word/review/page.tsx에 params 추가

--- src/app/(afterLogin)/word/[studyId]/review/page.tsx
+++ src/app/(afterLogin)/word/[studyId]/review/page.tsx
@@ -2,14 +2,22 @@ import { Suspense } from "react"
 import WordLearningContainer from "../_component/WordLearningContainer"
 import LoadingSpinner from "@/app/_component/LoadingSpinner"
 
-export default function WordReviewPage() {
+type Props = {
+  params: Promise<{
+    studyId: string;
+  }>;
+}
+
+export default async function WordReviewPage({ params }: Props) {
+  const { studyId } = await params;
+
   return (
     <Suspense fallback={
       <div className="flex justify-center items-center w-full h-screen">
         <LoadingSpinner size={"md"}/>
       </div>
     }>
-      <WordLearningContainer isReview={true}/>
+      <WordLearningContainer studyId={studyId} isReview={true}/>
     </Suspense>
   )
 }
