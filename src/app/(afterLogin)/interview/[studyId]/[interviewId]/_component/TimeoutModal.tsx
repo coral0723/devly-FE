9f7d03310604d589b95dcec756d37002be911059
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2025-12-27 15:04:27 +0900

    fix: TimeoutModal 배경 색 변경
    - bg-black -> bg-black/50으로 변경

--- src/app/(afterLogin)/interview/[studyId]/[interviewId]/_component/TimeoutModal.tsx
+++ src/app/(afterLogin)/interview/[studyId]/[interviewId]/_component/TimeoutModal.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function TimeoutModal({onClose}: Props) {
   
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-20">
+    <div className="fixed inset-0 bg-black/50 bg-opacity-50 flex items-center justify-center z-20">
       <div className="bg-white rounded-lg p-6 w-80">
         <h2 className="text-xl font-bold mb-4">시간 초과</h2>
         <p className="mb-6">면접 시간이 종료되었습니다.</p>
